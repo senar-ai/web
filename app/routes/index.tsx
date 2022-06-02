f785c41d6a22bed9d95afdd7343f79605f3d1c8e
Zain Fathoni <me@zainf.dev>
2022-06-03 02:30:16 +0800

    feat: show ringkasan

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -6,15 +6,18 @@ export default function Index() {
     <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
       <h1>Senarai belajar skill abad 21 untuk anak-anak</h1>
       <ul>
-        {activities.map(({ nama, link }) => (
+        {activities.map(({ nama, link, ringkasan }) => (
           <li key={nama}>
-            {link ? (
-              <a target="_blank" rel="noreferrer noopener" href={link}>
-                {nama}
-              </a>
-            ) : (
-              <div>{nama} </div>
-            )}
+            <details open={link}>
+              <summary>{nama}</summary>
+              {link ? (
+                <a target="_blank" rel="noreferrer noopener" href={link}>
+                  {ringkasan}
+                </a>
+              ) : (
+                <div>{ringkasan}</div>
+              )}
+            </details>
           </li>
         ))}
       </ul>
