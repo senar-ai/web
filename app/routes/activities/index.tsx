aa53398e9989bc5a2fc6fda64645a7f5ca94fd6c
Zain Fathoni <me@zainf.dev>
2022-06-03 23:54:51 +0800

    feat: render usia

--- app/routes/activities/index.tsx
+++ app/routes/activities/index.tsx
@@ -13,56 +13,65 @@ export default function Index() {
         <div className="max-w-7xl mx-auto pb-12 px-4 sm:px-6 lg:px-8">
           <div className="bg-white rounded-lg shadow px-5 py-6 sm:px-6">
             <ul role="list" className="divide-y divide-gray-200">
-              {activities.map(({ id, nama, link, ringkasan, benua, negara, provinsi, kabupaten, kecamatan, desa }) => {
-                const taxonomy = [benua, negara, provinsi, kabupaten, kecamatan, desa]
-                const reducedTaxonomy = taxonomy.reduce(
-                  (acc, curr) => (acc[acc.length - 1] === curr ? acc : [...acc, curr]),
-                  []
-                )
-                const [first, ...rest] = reducedTaxonomy
-                return (
-                  <li
-                    key={id}
-                    className="relative bg-white py-5 px-4 hover:bg-gray-50 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600"
-                  >
-                    <div className="flex justify-between space-x-3">
-                      <div className="min-w-0 flex-1">
-                        <a target="_blank" rel="noreferrer noopener" href={link} className="block focus:outline-none">
-                          <span className="absolute inset-0" aria-hidden="true" />
-                          <p className="text-sm font-medium text-gray-900 truncate">{nama}</p>
-                          {isAllEmptyString(taxonomy) ? null : (
-                            <nav className="flex py-2" aria-label="Taxonomy">
-                              <ol role="list" className="flex items-center space-x-1">
-                                <li>
-                                  <div className="flex items-center">
-                                    <div className="text-sm font-medium text-gray-500 hover:text-gray-700">{first}</div>
-                                  </div>
-                                </li>
-                                {rest.map((item) => (
-                                  <li key={item}>
+              {activities.map(
+                ({ id, nama, link, ringkasan, benua, negara, provinsi, kabupaten, kecamatan, desa, usia }) => {
+                  const taxonomy = [benua, negara, provinsi, kabupaten, kecamatan, desa]
+                  const reducedTaxonomy = taxonomy.reduce(
+                    (acc, curr) => (acc[acc.length - 1] === curr ? acc : [...acc, curr]),
+                    []
+                  )
+                  const [first, ...rest] = reducedTaxonomy
+                  return (
+                    <li
+                      key={id}
+                      className="relative bg-white py-5 px-4 hover:bg-gray-50 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600"
+                    >
+                      <div className="flex justify-between space-x-3">
+                        <div className="min-w-0 flex-1">
+                          <a target="_blank" rel="noreferrer noopener" href={link} className="block focus:outline-none">
+                            <span className="absolute inset-0" aria-hidden="true" />
+                            <p className="text-sm font-medium text-gray-900 truncate">
+                              {nama}
+                              <span className="flex-shrink-0 inline-block ml-2 px-2 py-0.5 text-green-800 text-xs font-medium bg-green-100 rounded-full">
+                                {usia}
+                              </span>
+                            </p>
+                            {isAllEmptyString(taxonomy) ? null : (
+                              <nav className="flex py-2" aria-label="Taxonomy">
+                                <ol role="list" className="flex items-center space-x-1">
+                                  <li>
                                     <div className="flex items-center">
-                                      <ChevronRightIcon
-                                        className="flex-shrink-0 h-5 w-5 text-gray-400"
-                                        aria-hidden="true"
-                                      />
-                                      <div className="ml-1 text-sm font-medium text-gray-500 hover:text-gray-700">
-                                        {item}
+                                      <div className="text-sm font-medium text-gray-500 hover:text-gray-700">
+                                        {first}
                                       </div>
                                     </div>
                                   </li>
-                                ))}
-                              </ol>
-                            </nav>
-                          )}
-                        </a>
+                                  {rest.map((item) => (
+                                    <li key={item}>
+                                      <div className="flex items-center">
+                                        <ChevronRightIcon
+                                          className="flex-shrink-0 h-5 w-5 text-gray-400"
+                                          aria-hidden="true"
+                                        />
+                                        <div className="ml-1 text-sm font-medium text-gray-500 hover:text-gray-700">
+                                          {item}
+                                        </div>
+                                      </div>
+                                    </li>
+                                  ))}
+                                </ol>
+                              </nav>
+                            )}
+                          </a>
+                        </div>
                       </div>
-                    </div>
-                    <div className="mt-1">
-                      <p className="text-sm text-gray-600">{ringkasan}</p>
-                    </div>
-                  </li>
-                )
-              })}
+                      <div className="mt-1">
+                        <p className="text-sm text-gray-600">{ringkasan}</p>
+                      </div>
+                    </li>
+                  )
+                }
+              )}
             </ul>
           </div>
         </div>
