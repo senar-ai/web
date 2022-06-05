978f11c1776a17bfbb2e7f71c4f61a744f51b316
Zain Fathoni <me@zainf.dev>
2022-06-05 12:34:49 +0800

    feat: add github shortlink

--- app/routes/go/$shortlink.ts
+++ app/routes/go/$shortlink.ts
@@ -6,6 +6,7 @@ const shortlinks = {
     'https://docs.google.com/spreadsheets/d/194RDJZ5aSe87GdsjEEJPcrINB18w40fkPNtGtKRDgWM/edit?resourcekey#gid=742951977',
   form: 'https://docs.google.com/forms/d/e/1FAIpQLSe3mkw1HYUkRklU_sRTk5qgdr_jItqxR7wAjP_W0uEU8HBRqA/viewform',
   db: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQUXjZ5M1lL9thjOEeNJxKsvE3VQ8i3XXUt8ryKWiya1HdapIcLuMUHKZ-VWECqCcX-cPKCZYzPXVvY/pubhtml',
+  github: 'https://github.com/senar-ai/web',
 }
 
 export const loader: LoaderFunction = async ({ params: { shortlink }, request }) => {
