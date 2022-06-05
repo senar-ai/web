22732bb376f44f3f179b64f351f5121a206466c8
Zain Fathoni <me@zainf.dev>
2022-06-05 09:50:42 +0800

    feat: integrate GTM

--- app/root.tsx
+++ app/root.tsx
@@ -34,6 +34,16 @@ export default function App() {
       <head>
         <Meta />
         <Links />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-47CSFLV9JK"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-47CSFLV9JK');
+        `,
+          }}
+        />
       </head>
       <body className="h-full">
         <Outlet />
