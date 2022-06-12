2e9eada7d53d927d271afefd08c4488ea49ea0fe
Zain Fathoni <me@zainf.dev>
2022-06-13 01:05:33 +0800

    build(ts): declare CSS & JSON modules in global.d.ts

--- global.d.ts
+++ global.d.ts
@@ -1,1 +1,9 @@
-declare module '*.css'
+declare module '*.css' {
+  const classes: { readonly [key: string]: string }
+  export default classes
+}
+
+declare module '*.json' {
+  const value: unknown
+  export default value
+}
